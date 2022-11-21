f22a88df6896e1fcc456f226906fec2ede67657f
Balakrishna Sankar <balushunk@gmail.com>
2022-11-21 23:04:55 +0530

    test

---
+++ src/test/java/myexe/z.java
@@ -0,0 +1,12 @@
+package myexe;
+
+public class z {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a ="Testleaf";
+System.out.println(a.length());
+System.out.println(a.length()-1);
+	}
+
+}
